1b1068ae0a4a8c31846797c4971adc507659dc21
Alan Yao <alanyao.training@gmail.com>
2021-08-19 16:13:42 -0500

    idk

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,7 +2,6 @@ import { Provider } from 'react-redux';
 import { store } from '../app/store';
 import 'styles/globals.css';
 import { Provider as AuthProvider } from 'next-auth/client';
-
 function MyApp({ Component, pageProps }) {
   return (
     <AuthProvider session={pageProps.session}>
